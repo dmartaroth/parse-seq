cabe6e91417617c6ac2f8629bf6610158407a5ee
Daniela Roth <dmroth@ualberta.ca>
2024-03-28 16:14:54 -0600

    Add topup seq data
    Change import script to preprocess second dataset for topup

--- e18_5_cb/docs/01_set-up-directories_import-data.R
+++ e18_5_cb/docs/01_set-up-directories_import-data.R
@@ -3,8 +3,12 @@
 # ## ################################### ## #
 
 # Downloaded Parse Biosciences pipeline output from UBC bioinformatics team for
-# Bmp2 ctrl and ncko cranial base samples.
-# Date: Tue Mar 05 09:53:11 2024 ------------------
+# Bmp2 ctrl and ncko cranial base samples (Feb 23) and topup seq (Mar 27)
+
+# In this script, will preprocess all four datasets (ctrl, ctrltopup, mut, 
+# muttopup) separately
+
+# Date: Thu Mar 28 15:49:49 2024 ------------------
 
 
 # Packages, directories, and functions ------------------------------------
@@ -17,7 +21,8 @@ source(here::here("e18_5_cb","docs","themes.R")) # load themes
 
 # Import data -------------------------------------------------------------
 
-## Control sample ----------------------------------------------------------
+## Control samples ----------------------------------------------------------
+# original dataset
 mat_path <- data.path.ctrl
 mat <- ReadParseBio(mat_path)
 
@@ -44,6 +49,34 @@ head(x=ctrl[[]])
 Idents(ctrl) <- "ctrl"
 ctrl$orig.ident <- "ctrl"
 head(x=ctrl[[]])
+
+# topup dataset
+mat_path <- data.path.topupctrl
+mat <- ReadParseBio(mat_path)
+
+table(rownames(mat) == "") # check to see if empty gene names are present, add name if so.
+# returns FALSE
+
+rownames(mat)[rownames(mat) == ""] <- "unknown"
+
+cell_meta <-
+  read.csv(paste0(mat_path, "/cell_metadata.csv"), row.names = 1) # read in cell meta data
+
+(ctrltopup <-
+    CreateSeuratObject(
+      mat,
+      min.features = 100,
+      min.cells = 100,
+      names.field = 0,
+      meta.data = cell_meta
+    ))
+
+ctrltopup$original <- ctrltopup$orig.ident
+ctrltopup$orig.ident <- NULL
+head(x=ctrltopup[[]])
+Idents(ctrltopup) <- "ctrltopup"
+ctrltopup$orig.ident <- "ctrltopup"
+head(x=ctrltopup[[]])
 
 ## Mutant sample ----------------------------------------------------------
 mat_path <- data.path.ncko
@@ -73,11 +106,42 @@ Idents(ncko) <- "ncko"
 ncko$orig.ident <- "ncko"
 head(x=ncko[[]])
 
+# topup mutant
+mat_path <- data.path.topupncko
+mat <- ReadParseBio(mat_path)
+
+table(rownames(mat) == "") # check to see if empty gene names are present, add name if so.
+# returns FALSE
+
+rownames(mat)[rownames(mat) == ""] <- "unknown"
+
+cell_meta <-
+  read.csv(paste0(mat_path, "/cell_metadata.csv"), row.names = 1) # read in cell meta data
+
+(nckotopup <-
+    CreateSeuratObject(
+      mat,
+      min.features = 100,
+      min.cells = 100,
+      names.field = 0,
+      meta.data = cell_meta
+    ))
+
+nckotopup$original <- nckotopup$orig.ident
+nckotopup$orig.ident <- NULL
+head(x=nckotopup[[]])
+Idents(nckotopup) <- "nckotopup"
+nckotopup$orig.ident <- "nckotopup"
+head(x=nckotopup[[]])
+
 # Quality control ---------------------------------------------------------
 prepro.plots(ctrl,ncko,figs)
+prepro.plots(ctrltopup,nckotopup,topupfigs)
 
 ctrl <- add_percent_mito(ctrl)
+ctrltopup <- add_percent_mito(ctrltopup)
 ncko <- add_percent_mito(ncko)
+nckotopup <- add_percent_mito(nckotopup)
 
 # Filtering low quality cells ---------------------------------------------
 # Select subset values from prepro.plots output, especially violin plots
@@ -89,19 +153,36 @@ filtered_ctrl <- subset(x = ctrl,
                           (nCount_RNA < 50000) &
                           (percent.mito < 0.15))
 
+filtered_ctrltopup <- subset(x = ctrltopup,
+                        subset = (nFeature_RNA > 200) & # value from seurat vignette
+                          (nFeature_RNA < 5500) &
+                          (nCount_RNA < 50000) &
+                          (percent.mito < 0.1))
+
 filtered_ncko <- subset(x = ncko,
                         subset = (nFeature_RNA > 200) &
                           (nFeature_RNA < 5500) &
                           (nCount_RNA < 70000) &
                           (percent.mito < 0.15))
 
+filtered_nckotopup <- subset(x = nckotopup,
+                        subset = (nFeature_RNA > 200) &
+                          (nFeature_RNA < 6500) &
+                          (nCount_RNA < 70000) &
+                          (percent.mito < 0.1))
+
 # Gene-level filtering
 filtered_ctrl <- genelvlfilt(filtered_ctrl)
 filtered_ncko <- genelvlfilt(filtered_ncko)
+filtered_ctrltopup <- genelvlfilt(filtered_ctrltopup)
+filtered_nckotopup <- genelvlfilt(filtered_nckotopup)
 
 filt.plots(filtered_ctrl,filtered_ncko,figs)
+filt.plots(ctrltopup,nckotopup,topupfigs)
 
 
 # Save filtered cells -----------------------------------------------------
 saveRDS(filtered_ctrl, file = paste0(data.output,"/filtered_",control,"_",sample,".Rds"))
 saveRDS(filtered_ncko, file = paste0(data.output,"/filtered_",mutant,"_",sample,".Rds"))
+saveRDS(filtered_ctrltopup, file = paste0(data.output,"/filtered_",control,"_",sample,"_topup",".Rds"))
+saveRDS(filtered_nckotopup, file = paste0(data.output,"/filtered_",mutant,"_",sample,"_topup",".Rds"))
--- e18_5_cb/docs/directories.R
+++ e18_5_cb/docs/directories.R
@@ -23,8 +23,11 @@ for (dir in dirs) {
 }
 dir.create(data.output <- here::here(home.path, "data-output"),recursive = TRUE)
 dir.create(figs <- here::here(home.path, "figures"),recursive = TRUE)
+dir.create(topupfigs <- here::here(home.path, "figures_topup"),recursive = TRUE)
 
 # Create subdirectories for control and mutant data
 data.path <- here::here(home.path, "raw-data")
 dir.create(data.path.ctrl <- here::here(data.path, paste(control, sep = "_", sample)))
 dir.create(data.path.ncko <- here::here(data.path, paste(mutant, sep = "_", sample)))
+dir.create(data.path.topupctrl <- here::here(data.path, paste(control, sep = "_", sample,"topup")))
+dir.create(data.path.topupncko <- here::here(data.path, paste(mutant, sep = "_", sample,"topup")))
